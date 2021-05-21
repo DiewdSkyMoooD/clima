0e8c570a91b44e38520719e6cc92d7a3135c7e18
DiewdSkyMoooD <cesar5807@hotmail.com>
2021-05-20 20:06:46 -0500

    finalizado

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const key="c41894cccfbdf501bd389fef3f8d8dcf";
 const d=document;
 const $btn =d.getElementById('btn');
-const $pais=d.getElementById('pais');
 const $estado=d.getElementById('estado');
 const $tarjeta =d.getElementById('tarjeta');
 const descripciones={
     "clear sky":"cielo despejado",
     "few clouds":"pocas nuves",
     "scattered clouds":"nuves dispersas",
+    "overcast clouds":"nublado",
     "broken clouds":"nuves rotas",
     "shower rain":"aguacero",
     "rain":"lluevioso",
@@ -19,7 +19,7 @@ const descripciones={
 
 $btn.addEventListener('click',()=>{
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${$estado.value},${$pais.value}&appid=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${$estado.value}&appid=${key}`)
     .then((res)=>res.ok?res.json():Promise.reject(res))
     .then((json)=>{
         console.log(json)
@@ -30,15 +30,12 @@ $btn.addEventListener('click',()=>{
         let primerh4=d.createElement('h4');
         let segundoh4=d.createElement('h4');
         let img=d.createElement('img');
-        h1.textContent=`Clima en ${json.name}-${json.sys.country}`;
+        h1.textContent=`${json.name}-${json.sys.country}`;
         primerh4.textContent=temperatura;
 
         for (let el in descripciones){
-            console.log(descripciones[el])
             json.weather[0].description===el?segundoh4.textContent=descripciones[el]:console.log("no encontrado");
         }
-
-       // segundoh4.textContent=json.weather[0].description;
         img.setAttribute('src',`./img/${json.weather[0].icon}.png`);
         fragmento.appendChild(h1);
         fragmento.appendChild(img)
@@ -49,11 +46,20 @@ $btn.addEventListener('click',()=>{
     .catch((err)=>{
         console.log(err)
         $tarjeta.innerHTML='';
+        let fragmento=d.createDocumentFragment();
         let h1=d.createElement('h1');
+        let h4=d.createElement('h4');
+        let img=d.createElement('img');
         h1.classList.add("text-center")
         h1.textContent="No se pudo realizar la busqueda";
-        $tarjeta.appendChild(h1);
+        img.setAttribute('src','./img/noencontrado.png');
+        h4.textContent="revise la busqueda e intentelo de nuevo"
+        fragmento.appendChild(h1);
+        fragmento.appendChild(img);
+        fragmento.appendChild(h4);
+        $tarjeta.appendChild(fragmento);
     })
+    $estado.value=""
 });
 
 function kelvin(kelvin){
